a8b77ba677e4b51f90a8161486c69e73306fddb7
Imaduddin Haetami <khattami240193@gmail.com>
2022-05-20 06:34:58 +0700

    Multiple squares moved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { useState, useRef } from "react";
 import { useSpring, animated } from "@react-spring/web";
 
+const colors = [
+  "#5778a4",
+  "#e49444",
+  "#d1615d",
+  "#85b6b2",
+  "#6a9f58",
+  "#e7ca60",
+  "#a87c9f",
+  "#f1a2a9",
+  "#967662",
+  "#b8b0ac"
+];
+
 export default function App() {
   const [spin, set] = useState(false);
   const end = useRef(0);
@@ -11,21 +24,25 @@ export default function App() {
 
   const handleSpin = () => {
     set(!spin);
-    end.current = end.current + 100;
+    end.current = end.current + 500;
   };
 
   return (
     <>
       <button onClick={handleSpin}>spin</button>
-      <animated.div
-        style={{
-          width: 80,
-          height: 80,
-          backgroundColor: "red",
-          borderRadius: 10,
-          ...styles
-        }}
-      />
+      <animated.div style={{ display: "flex", ...styles }}>
+        {colors.map((val) => (
+          <div
+            key={val}
+            style={{
+              width: 80,
+              height: 80,
+              backgroundColor: val,
+              borderRadius: 10
+            }}
+          />
+        ))}
+      </animated.div>
     </>
   );
 }
