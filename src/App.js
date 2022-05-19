01285d580dd5e0905b548fe7a4c5c4cf77a8423a
Imaduddin Haetami <khattami240193@gmail.com>
2022-05-20 05:24:47 +0700

    The rect is now moving.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import "./styles.css";
+import { useSpring, animated } from "@react-spring/web";
 
 export default function App() {
+  const styles = useSpring({
+    loop: { reverse: true },
+    from: { x: 0 },
+    to: { x: 100 }
+  });
+
   return (
     <>
       <button>spin</button>
-      <div className="rect">&nbsp;</div>
+      <animated.div
+        style={{
+          width: 80,
+          height: 80,
+          backgroundColor: "red",
+          borderRadius: 10,
+          ...styles
+        }}
+      />
     </>
   );
 }
