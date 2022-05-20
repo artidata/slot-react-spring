af708c351d5d061fe2c7bce3ba04ca355e31a0ee
Imaduddin Haetami <khattami240193@gmail.com>
2022-05-20 10:13:30 +0700

    Enable infinity spins.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useSpring, animated } from "@react-spring/web";
 const colors = ["#5778a4", "#e49444", "#d1615d", "#85b6b2", "#6a9f58"];
 
 export default function App() {
+  const [keys, setKeys] = useState([0, 1, 2, 3, 4]);
   const [spin, set] = useState(false);
   const end = useRef(0);
   const styles = useSpring({
@@ -12,22 +13,26 @@ export default function App() {
   });
 
   const handleSpin = () => {
+    setKeys((prev) => Array.from(Array(10), (_, x) => x - 9 + prev[4]));
     set(!spin);
-    end.current = end.current + 500;
+    end.current = end.current + 400;
   };
 
   return (
     <>
       <button onClick={handleSpin}>spin</button>
       <div style={{ width: 400, overflow: "clip" }}>
-        <animated.div style={{ display: "flex", ...styles }}>
-          {colors.map((val) => (
+        <animated.div style={{ position: "relative", height: 80, ...styles }}>
+          {keys.map((val) => (
             <div
               key={val}
               style={{
-                width: 80,
-                height: 80,
-                backgroundColor: val,
+                position: "absolute",
+                width: 76,
+                height: 76,
+                top: 2,
+                left: val * 80 + 2,
+                backgroundColor: colors.at(val % 5),
                 borderRadius: 10
               }}
             />
