49a405ddd07667d0b4cd8615287d0089d0bb5504
Imaduddin Haetami <khattami240193@gmail.com>
2022-05-20 10:57:36 +0700

    Better styling of the prizes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import { useSpring, animated } from "@react-spring/web";
 
-const colors = ["#5778a4", "#e49444", "#d1615d", "#85b6b2", "#6a9f58"];
+const colors = ["#a6cee3", "#b2df8a", "#fb9a99", "#fdbf6f", "#cab2d6"];
 
 export default function App() {
   const [keys, setKeys] = useState([0, 1, 2, 3, 4]);
@@ -36,10 +36,10 @@ export default function App() {
               key={val}
               style={{
                 position: "absolute",
-                width: 76,
-                height: 76,
-                top: 2,
-                left: val * 80 + 2,
+                width: 70,
+                height: 70,
+                top: 5,
+                left: val * 80 + 5,
                 backgroundColor: colors.at(val % 5),
                 borderRadius: 10
               }}
